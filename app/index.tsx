29680d4d9c07ce888265f2ca18d211546fedb5bf
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2024-03-18 05:25:10 -0400

    add: Adding more options to prettierc

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 // Imports for react types
-import type { FC } from "react";
+import type { FC } from 'react';
 
 // Imports for Expo and Reac Native libraries
-import { Redirect } from "expo-router";
+import { Redirect } from 'expo-router';
 
 const Home: FC = (): JSX.Element => {
 	// Redirection to the primary screen
-	return <Redirect href="/explore" />;
+	return <Redirect href='/explore' />;
 };
 
 export default Home;
