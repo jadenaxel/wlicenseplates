397c559f627b8611cef3c7af10e24cc0e1629490
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2024-03-18 05:03:55 -0400

    doc(Wrapper): Documentation for the wrapped file.

--- Wrapper.tsx
+++ Wrapper.tsx
@@ -1,7 +1,9 @@
 import type { FC } from "react";
 
+// Importing necessary components from react
 import { createContext, useReducer } from "react";
 
+// Initializing values to state
 const initialValue: any = {
 	Data: "",
 	ContinentData: "",
@@ -9,6 +11,7 @@ const initialValue: any = {
 	PlatesData: "",
 };
 
+//  Creating actions names and exporting them
 export const Actions: any = {
 	All: "All",
 	Continent: "Contient",
@@ -16,6 +19,8 @@ export const Actions: any = {
 	Plates: "Plates",
 };
 
+// Reducer function
+// Reducing values to make sure they only recieve a specific value
 const reducer = (state: any, action: any): any => {
 	switch (action.type) {
 		case Actions.All:
@@ -32,9 +37,12 @@ const reducer = (state: any, action: any): any => {
 	}
 };
 
+// Exporting the context
 export const Context: any = createContext({});
 
+// Wrapper component
 const Wrapper: FC<any> = ({ children }: any): JSX.Element => {
+	// Setting the state and dispatcher up with the initial state and the reducer.
 	const [state, dispatch] = useReducer(reducer, initialValue);
 
 	return <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>;
